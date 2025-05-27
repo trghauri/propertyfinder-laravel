7dc091a0c1424a0fa86b0f54476c257b7cf30899
DESKTOP-J7IHUF3\User <informtalal@gmail.com>
2025-05-27 21:06:48 +0500

    protected to public

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -15,27 +15,27 @@ class HttpClient
         $this->token = $token ?? config('propertyfinder.api_token');
     }
 
-    protected function get(string $endpoint, array $query = [])
+    public function get(string $endpoint, array $query = [])
     {
         return Http::withToken($this->token)->get($this->baseUrl . $endpoint, $query)->json();
     }
 
-    protected function post(string $endpoint, array $data = [])
+    public function post(string $endpoint, array $data = [])
     {
         return Http::withToken($this->token)->post($this->baseUrl . $endpoint, $data)->json();
     }
 
-    protected function patch(string $endpoint, array $data = [])
+    public function patch(string $endpoint, array $data = [])
     {
         return Http::withToken($this->token)->patch($this->baseUrl . $endpoint, $data)->json();
     }
 
-    protected function put(string $endpoint, array $data = [])
+    public function put(string $endpoint, array $data = [])
     {
         return Http::withToken($this->token)->put($this->baseUrl . $endpoint, $data)->json();
     }
 
-    protected function delete(string $endpoint)
+    public function delete(string $endpoint)
     {
         return Http::withToken($this->token)->delete($this->baseUrl . $endpoint)->json();
     }
